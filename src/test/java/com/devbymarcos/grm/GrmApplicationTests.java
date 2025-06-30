fc4a645f22452669e101efe33cb0dcb671cc0a7f
devbymarcos <marcoslopes.dev@gmail.com>
2025-06-30 15:32:10 -0300

    Update application properties for environment variable support; remove obsolete GrmApplicationTests class

--- src/test/java/com/devbymarcos/grm/GrmApplicationTests.java
+++ src/test/java/com/devbymarcos/grm/GrmApplicationTests.java
@@ -1,9 +1,9 @@
-package com.grm.grm;
+package com.devbymarcos.grm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = GrmApplication.class)
 class GrmApplicationTests {
 
 	@Test
